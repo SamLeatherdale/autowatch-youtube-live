88866dad05760372fe8d6f5a48ec9b4fe1d1c42a
Sam Leatherdale <SamLeatherdale@users.noreply.github.com>
2022-10-09 23:56:05 +1100

    Add support for logging in

--- src/client.ts
+++ src/client.ts
@@ -1,58 +1,104 @@
 import { Page } from "puppeteer";
 
-export async function findStreams(page: Page) {
-  // Check for livestreams
-  return await page.evaluate(() => {
-    console.log(`[agent] finding livestreams...`);
+type FindStreamResult = {
+  isLive: boolean;
+  url?: string;
+};
 
-    function getParentLink(el: HTMLElement) {
-      let parent: HTMLElement | null | undefined = el;
+type CheckStatusResult = {
+  isChannelPage: boolean;
+  loginUrl?: string;
+  isStream: boolean;
+  isStreamWaiting: boolean;
+  isStreamRewards: boolean;
+};
 
-      if (live) {
-        do {
-          parent = parent?.parentElement;
-        } while (parent && parent.tagName !== "A");
-      }
-      return (parent as HTMLAnchorElement | undefined)?.href;
-    }
+type Action = "findStreams" | "checkStatus";
+type ActionResult<T extends Action> = T extends "findStreams"
+  ? FindStreamResult
+  : CheckStatusResult;
+type DispatchParams = { action: Action };
 
-    const live = document.querySelector<HTMLElement>("[overlay-style=LIVE]");
-    let link;
-
-    if (live) {
-      link = getParentLink(live);
-    } else {
-      const scheduled = document.querySelector<HTMLElement>(
-        "[overlay-style=UPCOMING]"
-      );
-      link = scheduled ? getParentLink(scheduled) : undefined;
-    }
-
-    const result = {
-      isLive: !!live,
-      url: link,
-    };
-    console.log(`[agent]`, result);
-    return result;
-  });
+export async function checkStatus(page: Page): Promise<CheckStatusResult> {
+  return dispatchAction(page, "checkStatus");
 }
 
-export async function checkStatus(page: Page) {
-  return await page.evaluate(() => {
-    console.log(`[agent] checking status...`);
-    const offlineSlate =
-      document.querySelector<HTMLElement>(".ytp-offline-slate");
-    const rewardsButton = document.querySelector<HTMLElement>(
-      ".ytd-account-link-button-renderer"
-    );
+export async function findStreams(page: Page): Promise<FindStreamResult> {
+  return dispatchAction(page, "findStreams");
+}
 
-    const result = {
-      isChannelPage: !!document.querySelector("#channel-container"),
-      isStream: !!document.querySelector("#chatframe"),
-      isStreamWaiting: offlineSlate?.style?.display !== "none",
-      isStreamRewards: rewardsButton?.innerText === "CONNECTED",
-    };
-    console.log(`[agent]`, result);
-    return result;
-  });
+async function dispatchAction<T extends Action>(page: Page, action: T) {
+  return await page.evaluate<
+    [DispatchParams],
+    (params: DispatchParams) => ActionResult<T>
+  >(
+    (params): ActionResult<T> => {
+      function getParentLink(el: HTMLElement) {
+        let parent: HTMLElement | null | undefined = el;
+
+        while (parent && parent.tagName !== "A") {
+          parent = parent?.parentElement;
+        }
+        return (parent as HTMLAnchorElement | undefined)?.href;
+      }
+      function findStreams(): FindStreamResult {
+        console.log(`[agent] finding livestreams...`);
+
+        const live = document.querySelector<HTMLElement>(
+          "[overlay-style=LIVE]"
+        );
+        let link;
+
+        if (live) {
+          link = getParentLink(live);
+        } else {
+          const scheduled = document.querySelector<HTMLElement>(
+            "[overlay-style=UPCOMING]"
+          );
+          link = scheduled ? getParentLink(scheduled) : undefined;
+        }
+
+        const result = {
+          isLive: !!live,
+          url: link,
+        };
+        console.log(`[agent]`, result);
+        return result;
+      }
+
+      function checkStatus(): CheckStatusResult {
+        console.log(`[agent] checking status...`);
+        const offlineSlate =
+          document.querySelector<HTMLElement>(".ytp-offline-slate");
+        const rewardsButton = document.querySelector<HTMLElement>(
+          ".ytd-account-link-button-renderer"
+        );
+        const loginButton = document.querySelector<HTMLElement>(
+          '.ytd-masthead [href^="https://accounts.google.com"]'
+        );
+
+        const result = {
+          isChannelPage: !!document.querySelector("#channel-container"),
+          loginUrl: loginButton ? getParentLink(loginButton) : undefined,
+          isStream: !!document.querySelector("#chatframe"),
+          isStreamWaiting: offlineSlate?.style?.display !== "none",
+          isStreamRewards: rewardsButton?.innerText === "CONNECTED",
+        };
+        console.log(`[agent]`, result);
+        return result;
+      }
+
+      const { action } = params;
+      if (action === "findStreams") {
+        // @ts-expect-error Not sure how to fix this
+        return findStreams();
+      }
+      if (action === "checkStatus") {
+        // @ts-expect-error Not sure how to fix this
+        return checkStatus();
+      }
+      throw new Error(`Unknown action ${action}`);
+    },
+    { action }
+  );
 }
